e912378cc36f6cbf1ca51a3fdfdcfd8c7cc5ddf4
Pablo López Rodríguez <boopablo100@gmail.com>
2019-02-26 23:42:40 +0000

    Title

---
+++ src/MainWindow.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './MainWindow.css';
+
+class MainWindow extends Component {
+  render() {
+    return (
+        <div>
+            <h1 class='name'>PABLO LOPEZ</h1>
+            <h1 class='title'>SOFTWARE DEVELOPER</h1>
+        <div class="parallelogram"></div>
+        <div class="rectangle"></div>
+        </div>
+    );
+  }
+}
+
+export default MainWindow;

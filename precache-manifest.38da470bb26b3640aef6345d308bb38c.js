f0e568cdc8554dbe78198a91f8fb6654d28ff4ae
Pablo López Rodríguez <boopablo100@gmail.com>
2019-02-26 20:34:14 +0000

    Updates

---
+++ precache-manifest.38da470bb26b3640aef6345d308bb38c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "2d4bc21236173def64c6",
+    "url": "/static/js/main.2d4bc212.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "2d4bc21236173def64c6",
+    "url": "/static/css/main.40054b51.chunk.css"
+  },
+  {
+    "revision": "4838d7d79cd422852459a4f97aca15a0",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/lopezpablo.com/precache-manifest.e89adceeef441126e60c0dfd0e8f2bdb.js"
+  "/precache-manifest.38da470bb26b3640aef6345d308bb38c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/lopezpablo.com/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

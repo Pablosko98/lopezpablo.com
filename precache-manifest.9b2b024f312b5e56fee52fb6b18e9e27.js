9dfcbdd9236ab95a3ef82716ba7d4800c0a10e3a
Pablo Lopez Rodriguez <40284057@live.napier.ac.uk>
2020-02-12 13:12:09 +0000

    Updates

---
+++ precache-manifest.9b2b024f312b5e56fee52fb6b18e9e27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1141a74f9179a1379c720298677b939f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44db08fad1babbcbf72b",
+    "url": "/static/css/main.46cd9c6e.chunk.css"
+  },
+  {
+    "revision": "80b1e1f08e5d6c508267",
+    "url": "/static/js/2.36dc1dc5.chunk.js"
+  },
+  {
+    "revision": "0261e2dee4a7e8969069abb914658242",
+    "url": "/static/js/2.36dc1dc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44db08fad1babbcbf72b",
+    "url": "/static/js/main.69559bd0.chunk.js"
+  },
+  {
+    "revision": "cd2ed6a3d8095fddb194",
+    "url": "/static/js/runtime-main.e37e0258.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.988d7c2bb68c23ccd4d1c24a53495fdf.js"
+  "/precache-manifest.9b2b024f312b5e56fee52fb6b18e9e27.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
